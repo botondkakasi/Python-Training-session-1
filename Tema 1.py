f41c83404c7379039002a06819a5288fe0f23dbb
botondkakasi <57403742+botondkakasi@users.noreply.github.com>
2020-07-01 07:36:14 +0300

    Tema 1

---
+++ Tema 1.py
@@ -0,0 +1,15 @@
+#1. Reverse the order of the items in an array.
+
+a=[1,2,3,4,5]
+a.reverse()
+print(a)
+
+#2. Get the number of occurrences of var b in array a.
+a = [1, 1, 2, 2, 2, 2, 3, 3, 3]
+b=2
+print(a.count(b))
+
+#3. Given a sentence as string, count the number of words in it.
+a = 'ana are mere si nu are pere'
+splited = len(a.split())
+print(str(splited))
